c52eba42a061a61a6b32f84f53811b9d816a464d
Pedro-Duran <pedroav.duran@gmail.com>
2025-07-04 20:22:24 -0300

    adiciona migrations que criam tabela dia , remove schema mientien

--- src/main/java/com/panduran/mientien/repository/dia/DiaRepositoryImpl.java
+++ src/main/java/com/panduran/mientien/repository/dia/DiaRepositoryImpl.java
@@ -3,7 +3,7 @@ package com.panduran.mientien.repository.dia;
 public class DiaRepositoryImpl {
 
     public static final String CADASTRAR_DIA = """
-            INSERT INTO MIENTIEN.DIA (TEXTO_PT, TEXTO_ZH, PING_YING, CAMINHO_AUDIO, DATA)
+            INSERT INTO DIA (TEXTO_PT, TEXTO_ZH, PING_YING, CAMINHO_AUDIO, DATA)
                VALUES (:textoPt, :textoZh, :pingYing, :caminhoAudio, :data)
             """;
 }
