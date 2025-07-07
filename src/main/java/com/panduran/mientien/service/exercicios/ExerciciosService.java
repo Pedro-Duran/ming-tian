42265e96e0d155ed1d399cdbee42dcf6d57de071
Pedro-Duran <pedroav.duran@gmail.com>
2025-07-07 13:26:52 -0300

    featÃ:#01 adiciona util que concatena caracteres que possuem o mesmo significado

--- src/main/java/com/panduran/mientien/service/exercicios/ExerciciosService.java
+++ src/main/java/com/panduran/mientien/service/exercicios/ExerciciosService.java
@@ -1,8 +1,9 @@
-package com.panduran.mientien.service;
+package com.panduran.mientien.service.exercicios;
 
 import com.panduran.mientien.dto.ExerciciosBuscarIdeogramas;
 import com.panduran.mientien.entity.Dia;
 import com.panduran.mientien.repository.exercicios.ExerciciosRepository;
+import com.panduran.mientien.service.exercicios.exerciciosServiceUtils.ExerciciosServiceUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,8 @@ public class ExerciciosService {
 
     @Autowired
     ExerciciosRepository exerciciosRepository;
+    @Autowired
+    static ExerciciosServiceUtils exerciciosServiceUtils;
 
     public List<String> buscarIdeogramas(ExerciciosBuscarIdeogramas.Request request) {
       List<Dia> buscarPalavrasPorDiaPorData = exerciciosRepository.buscarIdeogramas(request.getData());
@@ -26,15 +29,18 @@ public class ExerciciosService {
         List<Dia> buscarPalavrasPorDiaPorData = exerciciosRepository.buscarIdeogramas(request.getData());
         List<ExerciciosBuscarIdeogramas.Response> listaDeDiasComPalavrasPorDia = new ArrayList<>();
 
-        buscarPalavrasPorDiaPorData.forEach(dia -> {
-            ExerciciosBuscarIdeogramas.Response response = new ExerciciosBuscarIdeogramas.Response();
-        response.setPalavrasTraduzidas(dia.getPalavrasTraduzidas());
-        response.setPingYing(dia.getPingYing());
-        listaDeDiasComPalavrasPorDia.add(response);
-        });
-
+        iteraDiaParaAdicionarDadosAoResponse(buscarPalavrasPorDiaPorData, listaDeDiasComPalavrasPorDia);
 
         return listaDeDiasComPalavrasPorDia;
 
+    }
+
+    private static void iteraDiaParaAdicionarDadosAoResponse(List<Dia> buscarPalavrasPorDiaPorData, List<ExerciciosBuscarIdeogramas.Response> listaDeDiasComPalavrasPorDia) {
+        buscarPalavrasPorDiaPorData.forEach(dia -> {
+            ExerciciosBuscarIdeogramas.Response response = new ExerciciosBuscarIdeogramas.Response();
+        response.setPalavrasTraduzidas(exerciciosServiceUtils.juntarCaracteresComMesmoSignificado(dia.getPalavrasTraduzidas()));
+        response.setPingYing(dia.getPingYing());
+        listaDeDiasComPalavrasPorDia.add(response);
+        });
     }
 }
